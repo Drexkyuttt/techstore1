6599847743987097f289db360ed5556e90715f6b
Drexkyuttt <drexlherjaybarbo@gmail.com>
2025-05-04 18:39:35 -0700

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -37,7 +37,7 @@ export default function About() {
           </div>
           <div className="relative h-96">
             <img 
-              src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=800&q=80" 
+              src="./images/download.jpg" 
               alt="About Us" 
               className="absolute inset-0 w-full h-full object-cover rounded-lg shadow-xl"
             />
